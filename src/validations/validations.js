a81042da40d91eee61de1374a9ab04f2cc1eeb3f
VILCHES PEREZ Ruben <ruben.vilches@soprasteria.com>
2021-09-16 23:46:41 +0200

    commit

--- src/validations/validations.js
+++ src/validations/validations.js
@@ -1,6 +1,5 @@
 export const emailValidation = ({ email }) => {
   const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-  console.log('emailvalidation', re.test(String(email).toLowerCase()))
   return re.test(String(email).toLowerCase());
 }
 
@@ -8,6 +7,5 @@ export const passwordValidation = ({ password }) => {
   console.log(password)
   // min 8 letter password, with at least a symbol, upper and lower case letters and a number
   var re = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
-  console.log('passwordvalidation', re.test(password))
   return re.test(password);
 }
